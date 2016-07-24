dba6327469cea72336fcb9b8f71858337897c32a
Eric Hahn <erichahn@vt.edu>
2016-07-24 14:14:49 -0400

    rename

--- ApiTest.py
+++ ApiTest.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import json
+import bottle
+from bottle import route, run, request, abort
+
 
 import GithubPullFile
 import Parse
 
 def main():
-    print "Test"
+    print("Test")
+    Parse.run_command(input_file)
 
 if __name__ == "__main__":
     main()
